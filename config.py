b65e06e0fbf789ab763f356da8c49f3a746aebe8
Vadym Lishchynskyi <ya.lishinskiy2017@gmail.com>
2023-06-26 20:28:58 +0300

    config remove renamed values

--- config.py
+++ config.py
@@ -8,7 +8,6 @@ class SystemConfig:
     TG_API_ID = int(os.environ["TG_API_ID"])
     TG_API_HASH = os.environ["TG_API_HASH"]
     TG_BOT_TOKEN = os.environ["TG_BOT_TOKEN"]
-    TG_CHANNEL_ID = int(os.environ["TG_CHANNEL_ID"])
 
     PHONE_NUMBER = os.environ["PHONE_NUMBER"]
 
